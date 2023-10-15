4dcec03b3717e71527bd8fe17784c3e87f6d41d3
YenAnChen <88221596+PaladinAn@users.noreply.github.com>
2023-10-15 19:56:31 +0800

    Create GO_note.py

---
+++ GO_note.py
@@ -0,0 +1,22 @@
+# lambda 變數1, 變數2, 變數3....:運算式
+
+Ex1.
+lambda x, y, z:x+y+z
+
+Ex2.(變數(值))
+book=lambda x, y, z:x+y+z
+print(book(1,2,3))
+
+Ex3. ((浪打函式)())
+print((lambda x, y, z:x+y+z)(1,2,3))
+
+Ex4.   串列= [1,2,4,2,5,6,7]
+      定義1 =定義2(浪打函數 變數:運算式,串列)
+number = [1,2,3,3,5,4,5,5]
+kb=ka(lambda a:a<3, number)   #條件判斷
+  print(kb)
+  print(list(ka))..
+
+
+# list() :清單列出
+# sorted() :排序
